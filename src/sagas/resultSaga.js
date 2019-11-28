00a8356f1f0d2382ec7d752453543e0c4ee6ea51
Sayantan Dey <sayantan.dey511@gmail.com>
2019-11-28 07:49:49 +0530

    links

--- src/sagas/resultSaga.js
+++ src/sagas/resultSaga.js
@@ -2,6 +2,7 @@ import { takeEvery } from 'redux-saga/effects';
 import { RESULT } from '../constants';
 
 function saveResultSaga(payload) {
+    //Call an endpoint for saving data ( In this example I am returning the same payload)
     return payload
 }
 
--- src/sagas/slotRunningSaga.js
+++ src/sagas/slotRunningSaga.js
@@ -2,6 +2,7 @@ import { takeEvery } from 'redux-saga/effects';
 import { SLOT_RUNNING_STATUS } from '../constants';
 
 function updateSlotRunningStatusSaga(payload) {
+     //Call an endpoint for saving data ( In this example I am returning the same payload)
     return payload;
 }
 
